16ab4976ed697423253c898d85da56b0c3a14970
kekemoto <kekemoto@moove.co.jp>
2024-04-02 06:42:07 +0000

    update

--- install.sh
+++ install.sh
@@ -31,8 +31,6 @@ install_asdf_plugin(){
     asdf plugin update "$name"
     asdf install "$name" "$version"
     asdf global "$name" "$version"
-  else
-     return
   fi
 }
 
@@ -46,17 +44,21 @@ require_command unzip
 
 if command -v asdf >/dev/null; then
   . "$HOME/.asdf/asdf.sh"
-  install_asdf_plugin nvim   latest https://github.com/richin13/asdf-neovim.git
-  install_asdf_plugin tmux   latest https://github.com/aphecetche/asdf-tmux.git
-  install_asdf_plugin node   latest https://github.com/asdf-vm/asdf-nodejs.git
-  install_asdf_plugin jq     latest https://github.com/lsanwick/asdf-jq.git
-  install_asdf_plugin fzf    latest https://github.com/kompiro/asdf-fzf.git
-  install_asdf_plugin fd     latest https://gitlab.com/wt0f/asdf-fd.git
+  install_asdf_plugin python 3.10.14 https://github.com/danhper/asdf-python.git
+  install_asdf_plugin nvim   0.9.5   https://github.com/richin13/asdf-neovim.git
+  install_asdf_plugin tmux   3.4     https://github.com/aphecetche/asdf-tmux.git
+  install_asdf_plugin node   21.7.1  https://github.com/asdf-vm/asdf-nodejs.git
+  install_asdf_plugin jq     1.7.1   https://github.com/lsanwick/asdf-jq.git
+  install_asdf_plugin fzf    0.48.1  https://github.com/kompiro/asdf-fzf.git
+  install_asdf_plugin fd     9.0.0   https://gitlab.com/wt0f/asdf-fd.git
   echo "installed asdf plugin"
 else
   install_asdf
   echo "installed asdf"
 fi
 
-cp ./.bashrc $HOME/
-cp -r ./nvim $HOME/.config/
+cp -f ./.bashrc $HOME/ || true
+mkdir -p $HOME/.config
+cp -fr ./nvim $HOME/.config/ || true
+
+echo "DONE"
